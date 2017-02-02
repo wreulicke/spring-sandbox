ca40d7a98d17c9baa786a02f9cbad300847533bd
Wreulicke <masaya2dies@gmail.com>
2017-02-03 08:12:22 +0900

    delete websocket endpoint

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,7 +1,5 @@
 const url1 = `ws://${location.host}${location.pathname}echo`;
-const url2 = `ws://${location.host}${location.pathname}test`;
 test(url1);
-test(url2);
 function test(url) {
   const ws= new WebSocket(url);
   const bind=(f, v)=> f.bind(null, v);
