a6b51ba352918e0fad05a796cf7aefe1e824c34b
wreulicke <saito.masaya@classmethod.jp>
2018-01-16 16:25:52 +0900

    polish implementation

--- failsafe/src/main/java/com/github/wreulicke/spring/SampleInterceptor.java
+++ failsafe/src/main/java/com/github/wreulicke/spring/SampleInterceptor.java
@@ -3,17 +3,19 @@ package com.github.wreulicke.spring;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.stereotype.Component;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.AsyncWebRequestInterceptor;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.context.request.WebRequestInterceptor;
 
-@Component
 @Slf4j
-public class SampleInterceptor implements WebRequestInterceptor {
+public class SampleInterceptor implements AsyncWebRequestInterceptor {
 	
 	@Override
 	public void preHandle(WebRequest request) throws Exception {
@@ -28,11 +30,24 @@ public class SampleInterceptor implements WebRequestInterceptor {
 	public void afterCompletion(WebRequest request, Exception ex) throws Exception {
 		Object object = request.getAttribute(SampleInterceptor.class.getName(), RequestAttributes.SCOPE_REQUEST);
 		if (object instanceof LocalDateTime == false) {
+			request.removeAttribute(SampleInterceptor.class.getName(), RequestAttributes.SCOPE_REQUEST);
 			throw new IllegalStateException();
 		}
 		LocalDateTime dateTime = (LocalDateTime) object;
-		log.info("throughput {}", Duration.between(dateTime, LocalDateTime.now()).toNanos());
+		if (request instanceof NativeWebRequest) {
+			NativeWebRequest nativeWebRequest = (NativeWebRequest) request;
+			HttpServletRequest servletRequest = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+			HttpServletResponse servletResponse = nativeWebRequest.getNativeResponse(HttpServletResponse.class);
+			log.info("URI {}, status code: {}, throughput: {} (ns)",
+					servletRequest.getRequestURI(),
+					servletResponse.getStatus(),
+					Duration.between(dateTime, LocalDateTime.now()).toNanos());
+		}
 		request.removeAttribute(SampleInterceptor.class.getName(), RequestAttributes.SCOPE_REQUEST);
+	}
+	
+	@Override
+	public void afterConcurrentHandlingStarted(WebRequest request) {
 	}
 	
 }
