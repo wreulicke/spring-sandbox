38ebfb8ce6a3084e9b7758b923297358cab10857
wreulicke <saito.masaya@classmethod.jp>
2018-03-01 04:20:20 +0900

    add user

---
+++ simple-jpa/src/main/java/com/github/wreulicke/simple/user/User.java
@@ -0,0 +1,36 @@
+package com.github.wreulicke.simple.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
