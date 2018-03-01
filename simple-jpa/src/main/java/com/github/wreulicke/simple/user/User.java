06d5137f1088f07f16ec7b52a4c799d925d5bffc
wreulicke <saito.masaya@classmethod.jp>
2018-03-02 01:45:36 +0900

    polish user attributes

--- simple-jpa/src/main/java/com/github/wreulicke/simple/user/User.java
+++ simple-jpa/src/main/java/com/github/wreulicke/simple/user/User.java
@@ -29,31 +29,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "users")
 public class User {
 
   @Id
   @GeneratedValue
   @Column
+  Long id;
+
+  @Column
   private String username;
 
   @Column
   private String password;
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
