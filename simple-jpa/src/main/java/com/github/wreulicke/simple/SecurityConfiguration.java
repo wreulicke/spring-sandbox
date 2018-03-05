72d0e222c2b6bd128bcaa5bdedda9e56f3c03ea2
wreulicke <saito.masaya@classmethod.jp>
2018-03-05 10:12:09 +0900

    remove unnecessary configuration

--- simple-jpa/src/main/java/com/github/wreulicke/simple/SecurityConfiguration.java
+++ simple-jpa/src/main/java/com/github/wreulicke/simple/SecurityConfiguration.java
@@ -23,16 +23,15 @@
  */
 package com.github.wreulicke.simple;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -50,12 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .authorizeRequests()
       .anyRequest()
       .authenticated();
-  }
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService)
-      .passwordEncoder(bcrpt());
   }
 
   @Bean
