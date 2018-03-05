28975e258c449462a0c848b720c411d128a08da0
wreulicke <saito.masaya@classmethod.jp>
2018-03-05 10:10:55 +0900

    polish schema

--- simple-jpa/src/main/resources/db/migration/V1__create_initial_tables.sql
+++ simple-jpa/src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users (
-    id BIGINT PRIMARY KEY,
-	username VARCHAR(128) UNIQUE NOT NULL,
+    id BIGINT not null AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(128) NOT NULL,
 	password VARCHAR(128) NOT NULL,
+	UNIQUE INDEX `username_UNIQUE` (`username`)
 )/*! CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
 
 
 CREATE TABLE user_authorities (
 	username VARCHAR(128) PRIMARY KEY,
-	authorities VARCHAR(128) NOT NULL,
+	authorities VARCHAR(128) NOT NULL
 )/*! CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
