3fea602b655f37575077c0559eb6a02bb89027ff
wreulicke <saito.masaya@classmethod.jp>
2018-03-05 10:14:19 +0900

    add configuration

---
+++ simple-jpa/src/main/java/com/github/wreulicke/simple/ObjectMapperCustomizer.java
@@ -0,0 +1,40 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Wreulicke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.wreulicke.simple;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+@Configuration
+public class ObjectMapperCustomizer implements Jackson2ObjectMapperBuilderCustomizer {
+
+  @Override
+  public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+    jacksonObjectMapperBuilder.modules(new Jdk8Module());
+  }
+
+}
