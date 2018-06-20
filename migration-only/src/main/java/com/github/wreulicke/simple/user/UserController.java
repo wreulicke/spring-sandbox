34fc601a6a6bd3b3b4e6cdb150fea453a5b6c771
wreulicke <saito.masaya@classmethod.jp>
2018-06-20 23:49:03 +0900

    init repository

---
+++ migration-only/src/main/java/com/github/wreulicke/simple/user/UserController.java
@@ -0,0 +1,154 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Wreulicke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.wreulicke.simple.user;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserRepository userRepository;
+
+  private final UserAuthoritiesRepository userAuthoritiesRepository;
+
+  private final PasswordEncoder encoder;
+
+  private final PlatformTransactionManager transactionManager;
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> create(@RequestBody CreateUserRequest request) {
+    User user = new User();
+    user.setUsername(request.getUsername());
+    user.setPassword(encoder.encode(request.getPassword()));
+
+    try {
+      return new TransactionTemplate(transactionManager).execute(status -> {
+        userRepository.save(user);
+        UserAuthorities userAuthorities = new UserAuthorities();
+        userAuthorities.setUsername(user.getUsername());
+        userAuthorities.setAuthorities(request.getAuthorities());
+        userAuthoritiesRepository.save(userAuthorities);
+        return ResponseEntity.ok(new UserResponse(user, userAuthorities));
+      });
+    } catch (DataIntegrityViolationException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT)
+        .build();
+    }
+  }
+
+
+  @DeleteMapping("/{username}")
+  @Transactional
+  public ResponseEntity<?> delete(@PathVariable("username") String username) {
+    Optional<User> userOpt = userRepository.findByUsername(username);
+    if (!userOpt.isPresent()) {
+      return ResponseEntity.notFound()
+        .build();
+    }
+
+    User user = userOpt.get();
+    userRepository.delete(user);
+    UserAuthorities userAuthorities = userAuthoritiesRepository.findByUsername(username)
+      .orElse(null);
+    return ResponseEntity.ok(new UserResponse(user, userAuthorities));
+  }
+
+
+  @PostMapping(path = "/{username}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> update(@PathVariable("username") String username, @RequestBody UpdateUserRequest request) {
+    Optional<User> userOpt = userRepository.findByUsername(username);
+    if (!userOpt.isPresent()) {
+      return ResponseEntity.notFound()
+        .build();
+    }
+
+    User user = userOpt.get();
+
+    try {
+      new TransactionTemplate(transactionManager).execute(status -> {
+        // TODO: イケテナイ
+        // 何がイケテナイかというと
+        // userAuthoritiesが存在しない場合にUpdate時に勝手に作られてしまう。
+        // デフォルトは"USER"なのに対して、ここでは空にしている。
+        // １個のEntityの方がよかったかもね
+        UserAuthorities userAuthorities = userAuthoritiesRepository.findByUsername(user.getUsername())
+          .orElseGet(() -> {
+            UserAuthorities authorities = new UserAuthorities();
+            authorities.setUsername(user.getUsername());
+            authorities.setAuthorities(Collections.emptySet());
+            return userAuthoritiesRepository.save(authorities);
+          });
+        request.update(user, userAuthorities, encoder);
+        return userRepository.save(user);
+      });
+    } catch (DataIntegrityViolationException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT)
+        .build();
+    }
+
+    UserAuthorities userAuthorities = userAuthoritiesRepository.findByUsername(username)
+      .orElse(null);
+    return ResponseEntity.ok(new UserResponse(user, userAuthorities));
+  }
+
+
+  @GetMapping(path = "/{username}")
+  @Transactional(readOnly = true)
+  public ResponseEntity<?> get(@PathVariable("username") String username) {
+    Optional<User> userOpt = userRepository.findByUsername(username);
+    if (!userOpt.isPresent()) {
+      return ResponseEntity.notFound()
+        .build();
+    }
+
+    User user = userOpt.get();
+    UserAuthorities userAuthorities = userAuthoritiesRepository.findByUsername(username)
+      .orElse(null);
+    return ResponseEntity.ok(new UserResponse(user, userAuthorities));
+  }
+
+}
---
+++ migration-only/src/test/java/com/github/wreulicke/simple/SimpleApplicationTests.java
@@ -0,0 +1,89 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Wreulicke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.wreulicke.simple;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.HttpCookie;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.wreulicke.simple.user.CreateUserRequest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestDatabaseConfiguration.class)
+@Slf4j
+public class SimpleApplicationTests {
+
+  @Autowired
+  TestRestTemplate template;
+
+  @Test
+  public void createUser() throws JsonProcessingException {
+    HttpHeaders headers = new HttpHeaders();
+    byte[] encoded = java.util.Base64.getEncoder()
+      .encode("admin:admin".getBytes(StandardCharsets.UTF_8));
+    headers.add(HttpHeaders.AUTHORIZATION, "Basic " + new String(encoded, StandardCharsets.UTF_8));
+
+    CreateUserRequest request = new CreateUserRequest("test", "test");
+    HttpEntity<CreateUserRequest> entity = new HttpEntity<>(request, headers);
+
+    ResponseEntity<String> responseEntity = template.postForEntity("/users", entity, String.class);
+    String value = responseEntity.getHeaders()
+      .get("Set-Cookie")
+      .iterator()
+      .next();
+    Optional<HttpCookie> cookieOpt = HttpCookie.parse(value)
+      .stream()
+      .filter(cookie -> cookie.getName()
+        .equals("XSRF-TOKEN"))
+      .findFirst();
+    headers.add("Cookie", "XSRF-TOKEN=" + cookieOpt.orElseThrow(RuntimeException::new)
+      .getValue());
+    headers.add("X-XSRF-TOKEN", cookieOpt.orElseThrow(RuntimeException::new)
+        .getValue());
+
+    responseEntity = template.exchange("/users", HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+    assertThat(responseEntity).returns(HttpStatus.OK, ResponseEntity::getStatusCode);
+  }
+}
